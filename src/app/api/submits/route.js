a9708dc0b4a18729cf74074bd2f529f50c925147
lutvi . <lutvian702@gmail.com>
2024-02-20 23:55:50 +0700

    validasi

--- src/app/api/submits/route.js
+++ src/app/api/submits/route.js
@@ -2,6 +2,7 @@ import {NextResponse} from 'next/server'
 import dotenv from 'dotenv'
 import {google} from 'googleapis'
 
+
 dotenv.config({
     path: '../../../../.env'
 })
@@ -9,7 +10,8 @@ dotenv.config({
 export async function POST(request) {
     try {
         console.log('start connect')
-        const auth = await new google.auth.GoogleAuth({
+
+        const auth = new google.auth.GoogleAuth({
             credentials: {
                 client_email: process.env.GOOGLE_CLIENT_EMAIL,
                 private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
@@ -22,16 +24,15 @@ export async function POST(request) {
         });
         console.log('connected')
         
-        const {name,message} = await request.json()
-        console.log(`Receive data name: ${name}, message: ${message}`)
-        
         const sheets = google.sheets({
             auth,
             version: 'v4'
         })
+        const {name,message} = await request.json()
+
         console.log(`configuration sheet`)
         
-        const res = await sheets.spreadsheets.values.append({
+        await sheets.spreadsheets.values.append({
             spreadsheetId: process.env.GOOGLE_SHEET_ID,
             range: 'record!A1:B1',
             valueInputOption: 'USER_ENTERED',
@@ -41,11 +42,11 @@ export async function POST(request) {
                 ]
             }
         })
+
         console.log(`finish`)
         return NextResponse.json({message: 'success'},{status: 200})
     } catch (error) {
         console.log(`handling error: ${error.message}`)
         return NextResponse.json({message: 'failed'},{status:500})
-        
     }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 'use client'
+import React from 'react'
 import { useState } from "react";
-
-
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+
   const [name, setName] = useState("")
   const [message, setMessage] = useState("")
   const [load, setLoad] = useState(false)
+  const [notif, setNotif] = useState("")
+  const router = useRouter()
 
   const clearForm = () => {
     setName("")
@@ -18,7 +21,27 @@ export default function Home() {
     setLoad(true)
 
     try {
-      const res = await fetch('http://localhost:3000/api/submits', {
+
+      if(!name && !message) {
+        setNotif(<div className='bg-red-500 text-white p-2'>isi yang bener dong</div>)
+        setLoad(false)
+        return
+      }
+
+      const kataToxic = ["tai","anjing","bangsat","kontol","memek","jancok","asu","kntl","ajg","benget sia","babi","ibab","mmk"]
+      // callback
+      const ceckName = kataToxic.some(cekname => name.includes(cekname))
+      const ceckMessage = kataToxic.some(cekmessage => message.includes(cekmessage))
+      if(ceckMessage || ceckName) {
+        setNotif(<div className='bg-red-500 text-white p-2'>banyak istighfar deh jari lu, jangan ngarep ke kirim ye tunggu aja ampe botak</div>)
+        return
+      }
+
+      if(name.length>7) {
+        setNotif(<div className='bg-blue-500 text-white p-2'>panjang amat nama lu {name} ini bukan formulir daftar bikin ktp</div>)
+      }
+
+      const res = await fetch('/api/submits', {
         method: "POST",
         cache: "no-store",
         headers: {
@@ -26,31 +49,32 @@ export default function Home() {
         },
         body: JSON.stringify({name,message})
       })
-  
-      if(!res.ok) {
-        console.log('gagal respon')
-        clearForm()
-        setLoad(false)
-        return
-      }
-
+      
+      router.refresh()
+      const named = name
       clearForm()
+      setNotif(<div className='bg-blue-500 text-white p-2'>ThankYou pesan nya udah gua terima nih {named}</div>)
       setLoad(false)
-      return
+
+      
     } catch (error) {
       console.log(error.message)
-      return
     }
   }
   return (
     <>
       <main className="bg-gray-100 min-h-screen">
-        <div className="max-w-5xl mx-auto py-16">
+        <div className="fixed w-full">
+          {notif}
+        </div>
+        <div className=" max-w-5xl mx-auto py-16">
           <h1 className="text-center font-bold">Pesan</h1>
-          <form onSubmit={formSubmit} className="flex items-center flex-col gap-2 justify-center">
-            <input value={name} onChange={e => setName(e.target.value)} type="text" name="name" id="name" className="shadow-md p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-md" placeholder="sapa lu ?" />
-            <input value={message} onChange={e => setMessage(e.target.value)} type="text" name="message" id="message" className="shadow-md p-2 focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-md" placeholder="pesan apa nih" />
-            {load ? (<button type="submit" className="w-64 rounded-md py-2 bg-gray-500 text-white cursor-not-allowed" disabled>loading...</button>):(<button type="submit" className="w-64 rounded-md py-2 bg-blue-500 text-white transition ease-in-out duration-300 hover:bg-blue-700 hover:scale-95">Kirim</button>)}
+          <form onSubmit={formSubmit} className="flex items-center justify-center bg-red-200 h-full">
+            <div className='flex flex-col gap-[15px]'>
+              <input autoComplete='false' value={name} onChange={e => setName(e.target.value)} type="text" name="name" id="name" className="shadow-md p-2 block w-[340px] sm:text-md border-gray-300 rounded-md" placeholder="sapa lu ?" />
+              <input autoComplete='false' value={message} onChange={e => setMessage(e.target.value)} type="text" name="message" id="message" className="shadow-md p-2 block w-[340px] sm:text-md border-gray-300 rounded-md" placeholder="pesan apa nih" />
+              {load ? (<button type="button" className="w-[340px] rounded-md py-2 bg-gray-500 text-white cursor-not-allowed" disabled>sebentar ye....</button>):(<button type="submit" className="w-[340px] rounded-md py-2 bg-blue-500 text-white transition ease-in-out duration-300 hover:bg-blue-700 hover:scale-95">Kirim</button>)}
+            </div>
           </form>
         </div>
       </main>
